d566c37d04a8129d6951045c9968d8987bcc191f
JH <dafr47@naver.com>
2023-10-27 12:29:31 +0900

    추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import SassComponent from "./SassComponent";
+// import SassComponent from "./SassComponent";
 import StyledComponent from "./Styled-Component";
 
 function App() {
   return (
     <>
-      <SassComponent />
-      {/* <StyledComponent /> */}
+      {/* <SassComponent /> */}
+      <StyledComponent />
     </>
   );
 }
